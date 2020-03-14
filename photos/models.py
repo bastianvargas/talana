a35564810702a85f762290702802efcc9ce68047
OSX <osx@MacBook-Pro-de-OSX.local>
2020-03-14 20:12:44 -0300

    add __str__ and save method in model photo and plural model name

--- photos/models.py
+++ photos/models.py
@@ -6,3 +6,13 @@ class Photo(models.Model):
     pet_name = models.CharField(max_length=50)
     photo = models.ImageField(upload_to='pet_photo')
     likes = models.IntegerField(default=0)
+
+    def __str__(self):
+            return '{}'.format(self.nick_name)
+
+    def save(self):
+            self.nick_name = self.nick_name.upper()
+            super(Photo, self).save()
+
+        class Meta:
+            verbose_name_plural = "Photos"
