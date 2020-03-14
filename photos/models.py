76fdaffeb70728ebaba4d5ad5479ae00485f9f7c
OSX <osx@MacBook-Pro-de-OSX.local>
2020-03-14 20:04:43 -0300

    reset cached gitignore

--- photos/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Photo(models.Model):
-
-    nick_name = models.CharField(max_length=50)
-    pet_name = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='pet_photo')
-    likes = models.IntegerField(default=0)
--- photos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-#views
-from .views import home, new_photo
-
-urlpatterns = [
-    path('', home, name="home"),
-    path('new-photo', new_photo, name="new_photo")
-]
